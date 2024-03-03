e9ed14feff200e175765e358e2b3abb8ce76dcba
riri404 <rama2001rifai@gmail.com>
2024-03-02 23:19:29 -0500

    part 2b 2.6

---
+++ Part 2/phonebook/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import Person from "./Components/person"
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  //state to help us access new name from <input?>
+  const [newName, setNewName] = useState('')
+  
+
+  const addPerson= (event) =>{
+    event.preventDefault()
+    const nameObj = {
+      name:newName,
+      id: persons.length+1
+    }
+    setPersons(persons.concat(nameObj))
+    setNewName('')
+  }
+
+  const handleNameChange =(event)=>{
+    console.log(event.target.value)
+    setNewName(event.target.value)
+
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+          name: <input value={newName} onChange={handleNameChange} />
+          <button type="submit">add</button>
+      </form>
+      
+      <h2>Numbers</h2>
+      <ul>
+            {persons.map(person=> <Person key={person.id} person={person}/>)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
